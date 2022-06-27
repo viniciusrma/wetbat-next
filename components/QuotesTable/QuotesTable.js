f76cbcc033265db74222836fb4dd35a5c542a823
Vinícius Morais <vinicius@cysrc.com>
2022-06-27 09:13:21 -0300

    improve transport info

--- components/QuotesTable/QuotesTable.js
+++ components/QuotesTable/QuotesTable.js
@@ -25,7 +25,7 @@ export default function QuotesTable({ quotes }) {
             <td>{quote.node.destination}</td>
             <td>{quote.node.returnDate}</td>
             <td>{quote.node.people}</td>
-            <td>{quote.node.transport.toString()}</td>
+            <td>{quote.node.transport.toString() == 'true' ? '✅' : '❌'}</td>
             <td>{quote.node.name}</td>
             <td>{quote.node.email}</td>
             <td>Actions</td>
